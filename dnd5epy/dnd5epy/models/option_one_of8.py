f014cea21fcd3455338e2e988352d4ce381f5118
Wesley <rademakerwesley@gmail.com>
2023-06-24 08:59:02 +0200

    one file was not updated

--- dnd5epy/dnd5epy/models/option_one_of8.py
+++ dnd5epy/dnd5epy/models/option_one_of8.py
@@ -20,7 +20,7 @@ import json
 
 from typing import Optional, Union
 from pydantic import BaseModel, Field, StrictFloat, StrictInt, StrictStr
-from openapi_client.models.api_reference import APIReference
+from dnd5epy.models.api_reference import APIReference
 
 class OptionOneOf8(BaseModel):
     """
